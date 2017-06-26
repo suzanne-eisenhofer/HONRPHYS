cb46eba9551d22d1cb31320c8508b8cb012ddc34
Suzanne <suzeisenhofer@googlemail.com>
2017-06-26 14:11:29 -0400

    commit of main.cpp

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>                   //this is a pre-processor director (includes file stream didn't make in this program). Groups commands (e.g. cout is command in this)
+#include <string>                     //this director allows for the commands for the string; the string compound type is defined
+
+using namespace std;                  //shows using all the standard functions in standard library
+
+int main()                            //function. int means using integers in calculations, main is the main function of the program
+{                                     // opening/closing squiggly brackets have the statements/instructions
+  cout << "Hello world!" << endl;   //instruction - all instructions end with ";"
+}
