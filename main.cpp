cbfdbc08fce63edd88b7f5f7c43c70e64392e0b8
Suzanne <suzeisenhofer@googlemail.com>
2017-06-26 14:53:05 -0400

    second commit main.cpp

--- main.cpp
+++ main.cpp
@@ -6,4 +6,25 @@ using namespace std;                  //shows using all the standard functions i
 int main()                            //function. int means using integers in calculations, main is the main function of the program
 {                                     // opening/closing squiggly brackets have the statements/instructions
   cout << "Hello world!" << endl;   //instruction - all instructions end with ";"
+                                        //"cout" is output stream object - send text to terminal. data from computer delivered out to the user.
+                                             // Hence "<<" on both sides of information to be delivered to user.
+                                       //"text" between means it will just be printed on the screen. "endl" is end line
+
+  int i=2;                          //int let's know calculations will work as integers. i is variable for 2
+  cout<< "i=" << i << endl;         // prints "i =" on screen and then gives data i (so as variable, will print 2 on screen)
+
+  double a=3.3;                     // double works with numbers w/ decimal points. a is variable
+  cout<< "a=" <<a<<endl;
+
+  int j = a*i;                      //variable j defined as a multiplied by i
+
+  cout<< "a*i =" << j << endl <<endl;     //prints a*i on screen and then displays numerical j (but only as an integer, therefore decimal is not included)
+
+
+
+
+  return 0;
+
+
+
 }
